4004da82084ad6bfd61d9c857733fb382d355874
Alexandros Mitsis <alexandros.mitsis@gmail.com>
2016-03-04 21:26:16 +0200

    corrected some values

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,15 +5,15 @@ USE_CAMERA_STUB := true
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_BOARD_PLATFORM := msm8226
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
-TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := e8lte
+TARGET_RECOVERY_FSTAB := device/lge/e8lte/recovery.fstab
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 earlyprintk androidboot.console=ttyHSL0 user_debug=31 msm_rtb.filter=0x37 androidboot.hardware=e8lte androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_BASE := 0x00000000
@@ -29,3 +29,5 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_PREBUILT_KERNEL := device/lge/e8lte/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+TW_THEME := portrait_hdpi
